966782eea08d70bd293274f3045497b2903b02e9
Bernabe <bernabe@goout.mx>
2017-07-04 17:45:08 -0500

    Created store with default data. [WIP] reducers.

---
+++ client/store.js
@@ -0,0 +1,21 @@
+import { createStore, compse } from "redux";
+import { syncHistoryWithStore } from "react-router-redux";
+import { browserHistory } from "react-router";
+
+// import root reducer
+import rootReducer from "./reducers/index";
+// import default data
+import comments from "./data/comments";
+import posts from "./data/posts";
+
+// create an object for default data
+const defaltState = {
+  comments,
+  posts
+};
+
+const store = createStore(rootReducer, defaltState);
+
+export const history = syncHistoryWithStore(browserHistory, store);
+
+export default store;
