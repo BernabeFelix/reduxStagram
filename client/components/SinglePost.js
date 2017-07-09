7072bf74534b52efd59e71651020b1067267bb53
Bernabe <bernabe@goout.mx>
2017-07-08 21:47:43 -0500

    Added PostGridElement to singlePost. [WIP] Comments Component

--- client/components/SinglePost.js
+++ client/components/SinglePost.js
@@ -1,10 +1,17 @@
 import React from "react";
+import PostGridElement from "./PostGridElement";
+import Comments from "./Comments";
 
 class SinglePost extends React.Component {
   render() {
+    const i = this.props.posts.findIndex(
+      post => post.code === this.props.params.postId
+    );
+
     return (
       <div className="single-photo">
-        <h2> I'm Single Post </h2>
+        <PostGridElement {...this.props} post={this.props.posts[i]} i={i} />
+        <Comments />
       </div>
     );
   }
